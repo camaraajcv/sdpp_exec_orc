a88db1cbbd77372933dc9aed67591c431b88e58b
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2025-01-05 23:11:33 +0000

    ertert

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,69 @@ import requests
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-
+dicionario_orgao = {
+    "CAMARA DOS DEPUTADOS": 1000,
+    "FUNDO ROTATIVO DA CAMARA DOS DEPUTADOS": 1901,
+    "SENADO FEDERAL": 2000,
+    "FUNDO ESPECIAL DO SENADO FEDERAL": 2001,
+    "TRIBUNAL DE CONTAS DA UNIAO": 3000,
+    "SUPREMO TRIBUNAL FEDERAL": 10000,
+    "SUPERIOR TRIBUNAL DE JUSTICA": 11000,
+    "JUSTICA FEDERAL": 12000,
+    "JUSTICA MILITAR": 13000,
+    "JUSTICA ELEITORAL": 14000,
+    "JUSTICA DO TRABALHO": 15000,
+    "JUSTICA DO DISTRITO FEDERAL E DOS TERRITORIOS": 16000,
+    "CONSELHO NACIONAL DE JUSTICA": 17000,
+    "PRESIDENCIA DA REPUBLICA": 20000,
+    "PRESIDENCIA DA REPUBLICA": 20101,
+    "MINISTERIO ADM.FED.REFORMA ESTADO-EM EXTINCAO": 20103,
+    "MINISTERIO DO PLANEJAMENTO E ORCAMENTO": 20113,
+    "FUNDO DE IMPRENSA NACIONAL": 20116,
+    "SECRETARIA NACIONAL DE POLITICAS P/MULHERES": 20122,
+    "CONTROLADORIA-GERAL DA UNIAO": 20125,
+    "FUNDACAO ESCOLA NACIONAL DE ADM. PUBLICA": 20202,
+    "AGENCIA NACIONAL DO CINEMA": 20203,
+    "AGENCIA NACIONAL DE AVIACAO CIVIL": 20214,
+    "COMISSAO NACIONAL DE ENERGIA NUCLEAR": 20301,
+    "NUCLEBRAS EQUIPAMENTOS PESADOS S/A": 20302,
+    "INDUSTRIAS NUCLEARES DO BRASIL S/A": 20303,
+    "AGENCIA ESPACIAL BRASILEIRA - AEB": 20402,
+    "FUNDACAO CASA DE RUI BARBOSA": 20403,
+    "FUNDACAO BIBLIOTECA NACIONAL": 20404,
+    "FUNDACAO CULTURAL PALMARES": 20408,
+    "INSTITUTO DO PATRIMONIO HIST. E ART. NACIONAL": 20411,
+    "FUNDACAO NACIONAL DE ARTES": 20412,
+    "EMPRESA BRASIL DE COMUNICACAO S.A.-EBC": 20415,
+    "CONSELHO NACIONAL DE DES.CIENT.E TECNOLOGICO": 20501,
+    "FINANCIADORA DE ESTUDOS E PROJETOS": 20502,
+    "SUPERINTENDENCIA DO DESENVOLV. DO NORDESTE": 20601,
+    "SUPERINTENDENCIA DO DESENVOLV. DA AMAZONIA": 20602,
+    "SUPERINTENDENCIA DA ZONA FRANCA DE MANAUS": 20603,
+    "INSTITUTO BRASILEIRO DE TURISMO": 20604,
+    "COMPANHIA DE DESENVOLVIMENTO DE BARCARENA": 20605,
+    "INST.BRAS.DO MEIO AMB.E DOS REC.NAT.RENOVAV.": 20701,
+    "FUNDO NACIONAL DE DESENVOLVIMENTO": 20924,
+    "MINISTERIO DA AGRICULTURA E PECUARIA": 22000,
+    "INSTIT. NAC. DE COLONIZACAO E REFORMA AGRARIA": 22201,
+    "EMPRESA BRASILEIRA DE PESQUISA AGROPECUARIA": 22202,
+    "CIA.DE DES.DOS VALES DO S.FRANC.E DO PARNAIBA": 22203,
+    "DEPARTAMENTO NAC.DE OBRAS CONTRA AS SECAS": 22204,
+    "COMPANHIA NACIONAL DE ABASTECIMENTO": 22211,
+    "CIA.DE ENTREPOSTOS E ARMAZENS GER.DE S.PAULO": 22500,
+    "FUNDO DE DEFESA DA ECONOMIA CAFEEIRA": 22905,
+    "FUNDACAO DE DEFESA ECONOMICA CAFEEIRA": 23000,
+    "MINISTERIO DA CIENCIA, TECNOLOGIA E INOVACAO": 24000,
+    "INSTITUTO NAC.DE TECNOLOGIA DA INFORMACAO-ITI": 24208,
+    "CENTRO NAC DE TECN ELETRONICA AVANCADA S/A": 24209,
+    "TELECOMUNICACOES BRASILEIRAS S/A": 24216,
+    "FUNDINCIAMENTO DE DESENVOLVIMENTO CIENTIFICIVO": 25901,
+    "MINISTERIO DA FAZENDA": 25000,
+    "COMISSAO DE VALORES MOBILIARIOS": 25203,
+    "SUPERVISOR CONC.NACIONAL - AGRICULTURA": 25902,
+    "SECRETARIA DA SAUDE E EDUCACAO": 25205,
+    "FUND.PREVISTO INSTITUTO TENTALP-VIADAGRANCE": 25220
+}
 # Acessar a variável de ambiente
 api_key = st.secrets["general"]["CHAVE_API_PORTAL"]
 
@@ -10,6 +72,10 @@ if api_key:
     print('Chave da API carregada com sucesso.')
 else:
     print('Erro: Chave da API não encontrada na variável de ambiente.')
+
+# Função para obter o código do órgão com base no nome
+def obter_codigo(orgao_nome):
+    return orgaos.get(orgao_nome, "Órgão não encontrado")
 
 # Função para buscar dados da API
 def fetch_data(year, orgao_code, orgao_superior_code, api_key=None):
@@ -71,10 +137,15 @@ def main():
     year = st.selectbox("Escolha o ano", [2024, 2023, 2022, 2021])
 
     # Entrada para o código do órgão
-    orgao_code = st.text_input("Digite o código do órgão")
+    orgao_selecionado = input("Selecione o nome do órgão: ")
+    orgao_code = obter_codigo(orgao_selecionado)
+    print(f"O código do órgão '{orgao_selecionado}' é: {orgao_code}")
+    
 
     # Entrada para o código do órgão superior
-    orgao_superior_code = st.text_input("Digite o código do órgão superior")
+    orgao_selecionado2 = input("Selecione o nome do órgão: ")
+    orgao_superior_code = obter_codigo(orgao_selecionado2)
+    print(f"O código do órgão '{orgao_selecionado2}' é: {orgao_superior_code}")
 
     # Verificar se os códigos foram fornecidos
     if not orgao_code or not orgao_superior_code:
