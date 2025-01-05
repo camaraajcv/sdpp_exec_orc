4b53d4248ecd401d538b6885a57417dda6ba21a6
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2025-01-05 23:41:17 +0000

    xcvxcvcxsfsdsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
+
 caminho_arquivo='arquivos/lista-de-orgaos.xls'
 
 # Função para ler os dados do Excel e criar o dicionário
